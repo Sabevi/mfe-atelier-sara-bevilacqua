c07a1b1787a9f9f89fce22c86c37188f0fa2f189
Sara Bevilacqua <sarabev6@gmail.com>
2025-02-13 09:10:35 +0100

    feat: header application

---
+++ header-mfe/src/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header style={{ background: '#f0f0f0', padding: '1rem' }}>
+      <h1>My MFE Header</h1>
+    </header>
+  );
+};
+
+export default Header;
